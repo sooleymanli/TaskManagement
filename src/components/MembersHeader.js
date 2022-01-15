9770f5bff5fcc48d49812affe4ed03991b471836
fuadsuleymanli <fsuleymanli92@gmail.com>
2022-01-15 22:56:15 +0400

    Up

--- src/components/MembersHeader.js
+++ src/components/MembersHeader.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../style/MembersHeader.css'
+import NewMember from "./NewMember";
+import axios from "axios";
 
 function MembersHeader(props) {
+
+    const [membersNew, setMembersNew] = useState([]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:3000/members')
+            .then(function (response) {
+                setMembersNew(response.data)
+
+
+            })
+
+    },[])
+
     return (
+
         <div className="members-header">
             <div className="members-header-left">
                 <span className="team-title">Product Design Team</span>
@@ -11,15 +27,22 @@ function MembersHeader(props) {
             </div>
             <div className="members-header-right">
                 <div className="avatars">
-                    <div className="member-img"></div>
-                    <div className="member-img"></div>
-                    <div className="member-img"></div>
-                    <div className="member-img"></div>
-                    <div className="member-img member-count"></div>
+                    {membersNew.map(function(e,index){
+                        if(index<4){
+                        return(
+                            <div className="member-img" key={e.member_id} style={{backgroundImage:`url(${e.member_pic})`}}></div>
+                        )}
+
+
+                    })}
+
+
+                    <div className="member-img member-count">+{membersNew.length-4}</div>
                 </div>
                 <div className="new-member">
-
+                    <span>+ New Memmber</span>
                 </div>
+                <NewMember />
 
             </div>
         </div>
